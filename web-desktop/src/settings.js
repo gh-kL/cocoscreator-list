2a17ac166c125f5d669bcdf8a56e7a2f0c505cef
kL <328549495@qq.com>
2019-07-20 16:17:41 +0800

    Build.

---
+++ web-desktop/src/settings.js
@@ -0,0 +1,67 @@
+window._CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {}
+    },
+    launchScene: "db://assets/Main.fire",
+    scenes: [
+        {
+            url: "db://assets/Main.fire",
+            uuid: "31Vtkc49VIup23t6eVxyV4"
+        },
+        {
+            url: "db://assets/TestLack.fire",
+            uuid: "f6n355+CVPIqQJccqfwVkv"
+        },
+        {
+            url: "db://assets/TestPage.fire",
+            uuid: "bfSUEslaZF4IKv2nP96+gh"
+        }
+    ],
+    packedAssets: {
+        "03fb7b07a": [
+            "29FYIk+N1GYaeWH/q1NxQO",
+            "62g0gETxxCnazjLtJEo9/t",
+            "9bvaMerUlDyary99mJa6xp",
+            "a4+CBx30lNy7w/iqBWmyg6",
+            "bfSUEslaZF4IKv2nP96+gh",
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "f0BIwQ8D5Ml7nTNQbh1YlS"
+        ],
+        "069f3ddde": [
+            "62g0gETxxCnazjLtJEo9/t",
+            "9bvaMerUlDyary99mJa6xp",
+            "a4+CBx30lNy7w/iqBWmyg6",
+            "f6n355+CVPIqQJccqfwVkv"
+        ],
+        "06ed226f0": [
+            "5cqXjf2bROFpdTWL+5HNZh",
+            "71VhFCTINJM6/Ky3oX9nBT",
+            "81j9XeZSRCb5y179hjnCX8",
+            "b4P/PCArtIdIH38t6mlw8Y",
+            "d8HsitJHxOYqo801xBk8ev",
+            "e8Ueib+qJEhL6mXAHdnwbi"
+        ],
+        "0bd3506f8": [
+            "29FYIk+N1GYaeWH/q1NxQO",
+            "31Vtkc49VIup23t6eVxyV4",
+            "62g0gETxxCnazjLtJEo9/t",
+            "9bvaMerUlDyary99mJa6xp",
+            "a4+CBx30lNy7w/iqBWmyg6",
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "f0BIwQ8D5Ml7nTNQbh1YlS"
+        ]
+    },
+    md5AssetsMap: {},
+    orientation: "",
+    debug: true,
+    subpackages: {}
+};
